c233484e1ec35e995a7ce2005629dc51e70a5f99
not-nakul <nakulahlawat99@gmail.com>
2023-04-30 11:58:51 +0530

    minor changes

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,23 @@
 import WideContainer from "../UI/WideContainer";
 import classes from "./Header.module.css";
 
+import githubLogo from "../../assets/github.png";
+
 function Header() {
   return (
     <header className={classes["header"]}>
       <WideContainer>
         <div className={classes["heading-container"]}>
           <h1 className={classes["heading"]}>React Movies</h1>
+
+          <a
+            href="https://github.com/not-nakul/react-movies"
+            target="_blank"
+            className={classes["nav-link"]}
+          >
+            <p>Code</p>
+            <img src={githubLogo} alt="Github" />
+          </a>
         </div>
       </WideContainer>
     </header>
